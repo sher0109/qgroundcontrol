c1aa304eb5c34dbb50831c337ab88fc3c82a923e
sher0109 <cafaro.matteo@libero.it>
2026-02-19 16:58:36 +0100

    fix(appimage): custom AppStream metadata to pass appstreamcli validation

--- custom/cmake/CustomOverrides.cmake
+++ custom/cmake/CustomOverrides.cmake
@@ -5,8 +5,11 @@
 # Application branding
 set(QGC_APP_NAME        "SVPGCS"                    CACHE STRING "" FORCE)
 set(QGC_ORG_NAME        "SVP"                       CACHE STRING "" FORCE)
-set(QGC_ORG_DOMAIN      "com.svp"                   CACHE STRING "" FORCE)
+set(QGC_ORG_DOMAIN      "github.com/sher0109/svp-gcs" CACHE STRING "" FORCE)
 set(QGC_APP_DESCRIPTION "SVP Ground Control Station" CACHE STRING "" FORCE)
+
+# Custom AppStream metadata (fixes URL and developer info for AppImage validation)
+set(QGC_APPIMAGE_METADATA_PATH "${CMAKE_SOURCE_DIR}/custom/res/deploy/linux/org.mavlink.qgroundcontrol.appdata.xml.in" CACHE FILEPATH "" FORCE)
 
 # Platform icons (add your actual icon files to custom/res/icons/ and uncomment)
 # set(QGC_MACOS_ICON_PATH        "${CMAKE_SOURCE_DIR}/custom/res/icons/custom.icns"  CACHE FILEPATH "" FORCE)
